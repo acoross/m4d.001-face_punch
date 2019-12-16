8f19b962459af82a089a6005c4fd30c35ae29369
Ilhwan Shin <acoross@gmail.com>
2019-12-16 18:43:53 +0900

    릴리즈 배포기능

--- deploy.bat
+++ deploy.bat
@@ -1,7 +1,7 @@
 @echo off
 if not exist "deploy" mkdir deploy
 del /f /q deploy\*.*
-robocopy /njs /np /njh /nfl /nc /ndl /ns x64/Debug deploy
+robocopy /njs /np /njh /nfl /nc /ndl /ns x64/Release deploy
 copy external\sfml\lib\*.dll deploy\
-copy external\msvc\debugCRT\*.dll deploy\
+copy external\msvc\releaseCRT\*.dll deploy\
 copy Resources\*.* deploy\
